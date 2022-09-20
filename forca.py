289fd074d967cd0c80b50c006c8e6f0a9a3d5178
Mateus.com <101688882+mat-afk@users.noreply.github.com>
2022-09-20 13:54:34 -0300

    Add files via upload

---
+++ forca.py
@@ -0,0 +1,9 @@
+print("-----------------------------------")
+print(">> Bem-vindo ao jogo da Forca!!! <<")
+print("-----------------------------------")
+
+
+
+print("\n." * 5)
+print("Fim de jogo")
+
